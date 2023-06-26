e233c32f9fcdc016f433bbc86d02b94fe3117c8e
iamcal77 <+>
2023-06-26 09:04:11 +0300

    assignment completed

--- index.js
+++ index.js
@@ -9,3 +9,32 @@ function ternaryCheckCity(){
 function switchOnCharmFromTip(){
   // Write your code here!
 }
+// Function: scuberGreetingForFeet()
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    return 'No can do.';
+  }
+}
+
+
+
+// Function: ternaryCheckCity()
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+// Function: switchOnCharmFromTip()
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
